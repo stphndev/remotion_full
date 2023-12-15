898d77476330fe07895b557f4d782dbcbe42db9a
stphndev <133531810+stphndev@users.noreply.github.com>
2023-12-15 01:22:49 +0000

    Refactor video sequence

--- remotion/bundle/MyComp/Video/VideoSequence.tsx
+++ remotion/bundle/MyComp/Video/VideoSequence.tsx
@@ -1,6 +1,6 @@
 import { linearTiming, TransitionSeries } from '@remotion/transitions'
 import { slide } from '@remotion/transitions/slide'
-import { Video, useVideoConfig, staticFile } from 'remotion'
+import { OffthreadVideo, useVideoConfig, staticFile } from 'remotion'
 
 export const VideoSequence = () => {
   const { width, height } = useVideoConfig()
@@ -8,7 +8,7 @@ export const VideoSequence = () => {
     <div style={{ position: 'relative', bottom: '10%' }}>
       <TransitionSeries>
         <TransitionSeries.Sequence durationInFrames={270}>
-          <Video
+          <OffthreadVideo
             src={staticFile('stock_market_1.mp4')}
             height={height / 2}
             width={width}
@@ -19,7 +19,7 @@ export const VideoSequence = () => {
           timing={linearTiming({ durationInFrames: 10 })}
         />
         <TransitionSeries.Sequence durationInFrames={270}>
-          <Video
+          <OffthreadVideo
             src={staticFile('stock_market_2.mp4')}
             height={height / 2}
             width={width}
@@ -30,7 +30,7 @@ export const VideoSequence = () => {
           timing={linearTiming({ durationInFrames: 10 })}
         />
         <TransitionSeries.Sequence durationInFrames={270}>
-          <Video
+          <OffthreadVideo
             src={staticFile('stock_market_3.mp4')}
             height={height / 2}
             width={width}
